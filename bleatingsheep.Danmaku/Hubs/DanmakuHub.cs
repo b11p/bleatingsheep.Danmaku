b809b085406b17a3c2b251fd81a790b3e5467a7d
bleatingsheep <bleatingsheep@live.cn>
2022-03-18 22:21:53 +0800

    feat: Add basic feature.

---
+++ bleatingsheep.Danmaku/Hubs/DanmakuHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace bleatingsheep.Danmaku.Hubs;
+
+public class DanmakuHub : Hub
+{
+    public async Task SendMessage(string group, string user, string message)
+    {
+        await Clients.OthersInGroup(group).SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task JoinGroup(string group)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, group);
+    }
+}
---
+++ bleatingsheep.Danmaku/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: "MyAllowSpecificOrigins",
+        builder =>
+        {
+            _ = builder
+            .WithOrigins("http://localhost:4000", "https://bleatingsheep.org")
+            .WithHeaders("x-requested-with", "x-signalr-user-agent")
+            .AllowCredentials();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseCors("MyAllowSpecificOrigins");
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
