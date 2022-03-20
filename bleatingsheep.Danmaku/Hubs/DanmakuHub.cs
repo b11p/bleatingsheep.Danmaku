027905754dc6b6a0fc85755867b1c7daacfe772c
bleatingsheep <bleatingsheep@live.cn>
2022-03-20 11:01:21 +0800

    feat: add logging to messages.

--- bleatingsheep.Danmaku/Hubs/DanmakuHub.cs
+++ bleatingsheep.Danmaku/Hubs/DanmakuHub.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.SignalR;
 
 namespace bleatingsheep.Danmaku.Hubs;
 
 public class DanmakuHub : Hub
 {
+    private readonly ILogger<DanmakuHub> _logger;
+
+    public DanmakuHub(ILogger<DanmakuHub> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task SendMessage(string group, string user, string message)
     {
         await Clients.OthersInGroup(group).SendAsync("ReceiveMessage", user, message);
+        _logger.LogInformation("{} ID {} sent: {}", user, Context.ConnectionId, message);
     }
 
     public async Task JoinGroup(string group)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        var httpConnectionFeature = Context.Features.Get<IHttpConnectionFeature>();
+        _logger.LogInformation("{} from {} joined group {}", Context.ConnectionId, httpConnectionFeature?.RemoteIpAddress, group);
     }
 
     public Task Connection(string group)
