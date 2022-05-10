5bc92e2270a5744900c58a2ecd824bc7e59c43fc
bleatingsheep <bleatingsheep@live.cn>
2022-05-11 00:35:13 +0800

    refactor: send user name instead of connection id.

--- bleatingsheep.Danmaku/Hubs/DanmakuHub.cs
+++ bleatingsheep.Danmaku/Hubs/DanmakuHub.cs
@@ -14,7 +14,7 @@ public class DanmakuHub : Hub
 
     public async Task SendMessage(string group, string user, string message)
     {
-        await Clients.OthersInGroup(group).SendAsync("ReceiveMessage", Context.ConnectionId, message);
+        await Clients.OthersInGroup(group).SendAsync("ReceiveMessage", user, message);
         _logger.LogInformation("{} ID {} sent: {}", user, Context.ConnectionId, message);
     }
 
